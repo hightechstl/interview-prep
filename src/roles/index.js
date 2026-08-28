a06fc242398001553bff47eb19edeb2be7f025e6
hightechstl <hightechstl@gmail.com>
2026-08-28 15:22:59 -0500

    Add ITIL certification prep track

--- src/roles/index.js
+++ src/roles/index.js
@@ -1,5 +1,6 @@
 import { infrastructureRole } from './infrastructure'
 import { supportSpecialistRole } from './support-specialist'
+import { itilFoundationRole } from './itil-foundation'
 
-export const roleCatalog = [infrastructureRole, supportSpecialistRole]
+export const roleCatalog = [infrastructureRole, supportSpecialistRole, itilFoundationRole]
 export const getRole = roleId => roleCatalog.find(role => role.id === roleId) || roleCatalog[0]
